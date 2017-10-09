a7d7a4f351eba358d4e0a25ec00dbdb16b281cd0
blingfeng <3394339422@qq.com>
2017-10-09 12:08:19 +0800

    second commit

--- src/main/java/cn/blingfeng/user/controller/PageController.java
+++ src/main/java/cn/blingfeng/user/controller/PageController.java
@@ -4,7 +4,6 @@ import cn.blingfeng.user.service.UserSerivce;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-ssssss
 @Controller
 public class PageController {
     @Autowired
