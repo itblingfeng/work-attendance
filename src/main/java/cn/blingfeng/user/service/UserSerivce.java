4053f570fd431e643f4aecc7bd3cb65e137ee3b8
blingfeng <3394339422@qq.com>
2017-10-10 08:58:26 +0800

    second commit

--- src/main/java/cn/blingfeng/user/service/UserSerivce.java
+++ src/main/java/cn/blingfeng/user/service/UserSerivce.java
@@ -3,6 +3,5 @@ package cn.blingfeng.user.service;
 import cn.blingfeng.user.pojo.User;
 
 public interface UserSerivce {
-    User findUserById();
     boolean checkAccount(User user);
 }
--- src/main/java/cn/blingfeng/user/service/impl/UserServiceImpl.java
+++ src/main/java/cn/blingfeng/user/service/impl/UserServiceImpl.java
@@ -3,10 +3,9 @@ package cn.blingfeng.user.service.impl;
 import cn.blingfeng.user.mapper.UserMapper;
 import cn.blingfeng.user.pojo.User;
 import cn.blingfeng.user.service.UserSerivce;
-import com.sun.mail.smtp.DigestMD5;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import sun.security.provider.MD5;
+import org.springframework.util.DigestUtils;
 
 import java.util.List;
 
@@ -15,21 +14,13 @@ public class UserServiceImpl implements UserSerivce{
     @Autowired
     private UserMapper userMapper;
     @Override
-    public User findUserById() {
-        User user = userMapper.selectByPrimaryKey(1l);
-        return user;
-    }
-
-    @Override
     public boolean checkAccount(User user) {
         List<User> userList = userMapper.selectUserByUsername(user.getUsername());
         if(userList==null||userList.size()==0)
             return false;
          User realUser = userList.get(0);
 //        将user的密码MD5加密后与password进行比较
-        if(realUser.getPassword().equals(user.getPassword())){
-            return true;
-        }
-        return false;
+        String md5Password = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        return md5Password.equals(realUser.getPassword());
     }
 }
